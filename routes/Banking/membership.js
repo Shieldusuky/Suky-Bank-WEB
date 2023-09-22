1724f547576d967726e5c85a433824216d457da8
김유진 <yjin2006@gmail.com>
2023-09-22 14:31:14 +0900

    데이터베이스 수정이 필요합니다. 필요한 SQL: ALTER TABLE users ADD membership VARCHAR(25) DEFAULT 'FRIEND' NOT NULL; UPDATE users SET membership = 'ADMIN' WHERE username = 'admin';
    (edited #1)
    
    (edited #2)

--- routes/Banking/membership.js
+++ routes/Banking/membership.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var axios = require("axios");
 var {decryptRequest} = require("../../middlewares/crypt");
 const profile = require('../../middlewares/profile');
-const checkCookie = require("../../middlewares/checkCookie")
+const checkCookie = require("../../middlewares/checkCookie");
 
 router.get('/', checkCookie, function (req, res) {
     const cookie = req.cookies.Token;
@@ -16,18 +16,46 @@ router.get('/', checkCookie, function (req, res) {
             headers: {"authorization": "1 " + cookie}
         }).then((data) => {
             let result = decryptRequest(data.data).data;
-            var html_data = `<thead>
-                            <tr>
-                                <th>멤버십 번호</th>
-                            </tr>
-                            </thead>
-                            
-                            <tbody>
-                            <tr>
-                                <td>${result.membership}</td>
-                            </tr>
-                            </tbody>`
+            var html_data = ``;
+            
+            if(result.membership == "FRIEND") {
+                html_data = `<h2 align='center'>회원님의 멤버십 등급은 FRIEND등급입니다.</h2>
+                <p align='center'>멤버십 등급이 높아지면 지금보다 더 많은 특별한 혜택을 경험할 수 있습니다.</p>`
+            } else if (result.membership == "ADMIN") {
+                html_data = `<h2 align='center'>당신은 관리자입니다.</h2>
+                <p align='center'>환영합니다, 관리자님!</p>`       
+            } else {
+                html_data = `<h2 align='center'>회원님의 멤버십 등급은 ${result.membership}등급입니다.</h2>
+                <p align='center'>축하합니다! 이제 ${result.membership}등급의 모든 혜택을 경험할 수 있습니다.</p>`
+            }
 
+            html_data += `<thead>
+            <tr>
+                <th>회원등급</th>
+                <th>FRIEND</th>
+                <th>GOLD</th>
+                <th>PREMIUM</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td><b>누적 이용 횟수</b></td>
+                <td>최초 가입</td>
+                <td>100회 이상</td>
+                <td>100,000회 이상</td>
+            </tr>
+            <tr>
+                <td><b>회원 혜택</b></td>
+                <td>포인트 적립 가능</td>
+                <td>포인트 적립 시 3% 추가 적립<br>이체 수수료 3% 할인</td>
+                <td>포인트 적립 시 10% 추가 적립<br>이체 수수료 10% 할인</td>
+            </tr>
+            <tr>
+                <td><b>비고</b></td>
+                <td colspan="3">멤버십 페이지를 모의해킹에 쓸 수 있을지 궁금합니다.</td>
+            </tr>
+            </tbody>
+            `
             return res.render("Banking/membership", {html: html_data, pending: pending, select: "membership"});
         }).catch(function (error) {
             var html_data = "<tr>에러</tr>";
