dbcab626ce2bf6949f795be3b38448439df8f6de
DESKTOP-FIM6H62\USER <0721juli@naver.com>
2023-10-04 15:14:37 +0900

    인증_89프로 수정

--- routes/User/smsAuth.js
+++ routes/User/smsAuth.js
@@ -1,33 +1,37 @@
 var express = require('express');
 var router = express.Router();
-
-const {encryptResponse, decryptRequest} = require('../../middlewares/crypt')
+const Response = require("../../middlewares/Response");
+const {decryptRequest, decryptEnc, encryptResponse} = require("../../middlewares/crypt");
 const axios = require("axios");
 const sha256 = require("js-sha256")
 
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    // res.render("temp/findPass");
     res.render("temp/smsAuth", {select: "smsAuth"});
 });
 
-router.post('/', async function (req, res, next) {
-    const {username, smsauth} = req.body;
-    const baseData = `{"username": "${username}", "authnum" : "${smsauth}"}`;
-    const enData = encryptResponse(baseData);
+router.post('/', (req, res) => {
+    const username = req.body.username;
+    const authnum = req.body.authnum;
+    const baseData = `{"username": "${username}", "authnum" : "${authnum}"}`;
+    let resStatus = ""
+    let resMessage = ""
 
     axios({
         method: "post",
-        url: api_url + "/api/user/smsAuth",
-        data: enData
+        url: api_url + "/api/User/smsAuth",
+        data: encryptResponse(baseData)
     }).then((data) => {
-        let result = decryptRequest(data.data);
+        resStatus = decryptRequest(data.data).status
+        resMessage = decryptRequest(data.data).data.message
 
-        if (result.status.code == 200) {
-            return res.send("<script>alert('인증이 완료되었습니다.');location.href = \"/user/findPassnext\";</script>");
+        if (resStatus.code == 200) {
+            //return res.send("<script>alert('인증이 완료되었습니다.');location.href = \"/user/findPassnext\";</script>");
+            return res.send(`<script>alert('인증이 완료되었습니다.');location.href = \"/user/findPassnext?username=${username}\";</script>`);
         } else {
-            return res.send("<script>alert('인증에 실패하였습니다.');location.href = \"/user/findPass\";</script>");
+            res.render("temp/findPass", {select: "login", message: resMessage})
+            // return res.send("<script>alert('인증에 실패하였습니다.');location.href = \"/user/findPass\";</script>");
         }
     })
 });
