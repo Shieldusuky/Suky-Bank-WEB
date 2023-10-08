47f616a8a837380e4604bd4a9d87c93b12944bef
DESKTOP-FIM6H62\USER <hwanghsun@naver.com>
2023-10-08 11:18:04 +0900

    1. 모바일에서 올린 글 삭제 안되어가지고 deleteBoard 수정함

--- routes/notice/deleteBoard.js
+++ routes/notice/deleteBoard.js
@@ -15,16 +15,20 @@ router.get('/', function (req, res, next) {
                     if (error) {
                         throw error;
                     }
-                    var fp = results[0].filepath
+                    // var fp = results[0].filepath; // 이 부분 주석 처리
                     db.query(`DELETE
                               FROM notices
                               WHERE id = ${req.query.id}`, function (error, results) {
                         if (error) {
                             throw error;
                         }
-                        fs.unlink(fp, err => {
-                        })
-                        res.redirect('viewBoard');
+                        // fs.unlink(fp, err => {  // 이 부분 주석 처리
+                        //     if (err) {
+                        //         console.error(err);
+                        //         // 파일 삭제 실패 시 처리
+                        //     }
+                            res.redirect('viewBoard'); 
+                        // });
                     });
                 });
             } else {
