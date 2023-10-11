e8de2aead2b56177b9530e2c248b121e527105ab
ansehd1123 <qkek8019@gmail.com>
2023-10-11 14:05:52 +0900

    라이트 보드 수정

--- routes/notice/writeBoard.js
+++ routes/notice/writeBoard.js
@@ -7,12 +7,14 @@ var {decryptEnc,} = require("../../middlewares/crypt");
 const profile = require("../../middlewares/profile");
 const multer = require("multer");
 const checkCookie = require("../../middlewares/checkCookie");
+var request = require("request");
+const fs = require("fs");
 
 const upload = multer({
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
             console.log(req.body.fid);
-            cb(null, req.body.fid);
+            cb(null, "./");
         },
         filename: function (req, file, cb) {
             cb(null, file.originalname);
@@ -41,41 +43,44 @@ router.get("/", function (req, res, next) {
 router.post(
     "/write",
     checkCookie,
-    upload.single("imgimg"),
+    upload.single("imgimg"), // 파일 업로드 설정
     function (req, res, next) {
-        //파일 안넣으면 오류나서 변경함
-        let filepath = "";
-        let destination = "";
+        const { title, contents } = req.body;
 
-        if (req.file) {
-            destination = req.file.destination;
-            if (destination) {
-                filepath = destination + "/" + req.file.filename;
-            } else {
-                filepath = req.file.filename;
-            }
-        } else {
-            filepath = null;
-            destination = null;
-        }
-
-        const {title, contents} = req.body;
-
+        // 클라이언트의 쿠키를 사용하여 사용자 프로필 정보를 가져옵니다.
         const cookie = req.cookies.Token;
         profile(cookie).then((data) => {
             var userId = data.data.username;
 
-            db.query(
-                `INSERT INTO notices
-                 VALUES (NULL, '${userId}', '${title}', '${contents}', '${filepath}', '${seoultime}', '${seoultime}
-                          ')`,
-                function (error, results) {
-                    if (error) {
-                        throw error;
-                    }
-                    res.redirect("../viewBoard");
+            // 파일 업로드는 그대로 유지
+
+            // 데이터를 다른 서버로 전송
+            request.post({
+                url: 'http://127.0.0.1:3000/api/notice/test/', // 변경된 API 주소
+                formData: {
+                    title: title,
+                    contents: contents,
+                    userId: userId,
+                    imgimg: fs.createReadStream(req.file.path), // 파일 업로드 필드와 파일 경로
+                },
+            }, function (error, response, body) {
+                if (error) {
+                    // 오류 처리
+                    throw error;
                 }
-            );
+
+                // 데이터베이스에 데이터를 삽입
+                db.query(
+                    `INSERT INTO notices
+                     VALUES (NULL, '${userId}', '${title}', '${contents}', '${req.file.originalname}', '${seoultime}', '${seoultime}')`,
+                    function (error, results) {
+                        if (error) {
+                            throw error;
+                        }
+                        res.redirect("../viewBoard");
+                    }
+                );
+            });
         });
     }
 );
